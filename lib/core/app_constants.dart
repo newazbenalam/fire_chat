6b9072de31b6e8531756bb3608fc317215ea712a
newazbenalam <newazbenalam@gmail.com>
2024-06-09 04:08:10 +0600

    add: colors

---
+++ lib/core/app_constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const String appName = "Fire Chat";
+
+const Color backgroundPrimaryColor = Color.fromARGB(255, 238, 234, 245);
+const Color primaryColor = Color(0xFF6200EE);
+const Color primaryColorLight = Color.fromARGB(255, 210, 196, 245);
+const Color primaryColorDark = Color(0xFF3700B3);
+const Color secondaryColor = Color(0xFF03DAC6);
+const Color secondaryColorLight = Color(0xFF03DAC6);
+const Color secondaryColorDark = Color(0xFF03DAC6);
+const Color primaryTextColor = Color(0xFF000000);
+const Color secondaryTextColor = Color(0xFFFFFFFF);
+const Color backgroundColor = Color(0xFFFFFFFF);
+const Color errorColor = Color(0xFFB00020);
+const Color successColor = Color(0xFF00C853);
+const Color warningColor = Color(0xFFFFD600);
+const Color infoColor = Color(0xFF2979FF);
+const Color disabledColor = Color(0xFFBDBDBD);
+const Color shadowColor = Color(0x95E9EBF0);
+const Color borderColor = Color(0xFFE0E0E0);
