4493a9450d2451cc428125710f25337c0e665de5
newazbenalam <newazbenalam@gmail.com>
2024-06-17 04:33:43 +0600

    remove: deps

--- lib/core/init_controllers.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fire_chat/features/login/presentation/getx/login_controller.dart';
-import 'package:get/get.dart';
-
-Future<void> init() async {
-  // Get.put(HomeScreenController());
-  Get.lazyPut(() => LoginController());
-}
