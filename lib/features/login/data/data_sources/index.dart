6f302e048b4f6eeb7f1abc09bb61ad020efa780b
newazbenalam <newazbenalam@gmail.com>
2024-06-17 04:44:15 +0600

    remove: deprecated

--- lib/features/login/data/data_sources/index.dart
+++
@@ -1,3 +0,0 @@
-// Export files for the data_sources of login
-export 'remote_data_source.dart';
-export 'local_data_source.dart';
--- lib/features/login/data/repositories/index.dart
+++
@@ -1,2 +0,0 @@
-// Export files for the repositories of login
-export 'login_repository_impl.dart';
