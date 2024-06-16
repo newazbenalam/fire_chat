543c96935010e9b2478c875da6ce6c9d25eb9e6c
newazbenalam <newazbenalam@gmail.com>
2024-06-16 18:48:17 +0600

    add: firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,43 @@ import 'package:fire_chat/core/app_constants.dart';
 import 'package:fire_chat/core/initilization.dart';
 import 'package:fire_chat/features/home/presentation/screens/home_screen.dart';
 import 'package:fire_chat/features/login/presentation/screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
-  coreInit();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await coreInit();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: appName,
+      title: 'Fire Chat',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const HomeScreen(title: 'Fire Chat'),
-      home: const LoginScreen(),
+      home: Scaffold(
+        backgroundColor: backgroundPrimaryColor,
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              return const HomeScreen(title: 'FireChat');
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
+      ),
     );
   }
 }
